0dfd64cf12b91bba49c65a4a71712375a59f747a
Ewelina <ewelinajazurek@gmail.com>
2021-06-17 17:53:42 +0200

    learn basic functions

---
+++ Javascript_course/function.js
@@ -0,0 +1,38 @@
+
+function addNumbers(arg1, arg2) {
+    return arg1 + arg2;
+}
+
+const result = addNumbers(2, 2);
+
+console.log(result);
+
+
+
+
+/**
+ * Convert minutes into seconds
+ * @param {number} minutes 
+ * @returns number with seconds
+ */
+function convertMinutesIntoSeconds(minutes) {
+    return minutes * 60;
+}
+
+
+const seconds = convertMinutes(5);
+
+console.log(seconds);
+
+
+
+// Power Calculator
+function powerCalculator(num1, num2) {
+return num1 * num2; 
+
+}
+
+const circuitPower = powerCalculator(230,10);
+
+console.log(circuitPower); 
+
