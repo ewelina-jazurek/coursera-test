eab6a34beb3197e6b842b1bf8694b88417694e59
Ewelina <ewelinajazurek@gmail.com>
2021-06-18 17:38:51 +0200

    adding swithch plus minus

---
+++ Javascript_course/calculator/calculator.js
@@ -0,0 +1,43 @@
+(function () {
+
+
+  const firstArgInput = document.getElementById('firstArg');
+  const secondArgInput = document.getElementById('secondArg');
+  const operation = document.getElementById('operation');
+
+  registerCalculateClick();
+  
+  
+  
+  
+  
+  function registerCalculateClick() {
+    const calculateBtn = document.getElementById('calculateBtn');
+    
+    calculateBtn.addEventListener('click', (clickEvent) => {
+      switch(operation.value) {
+        case 'plus': 
+        setResult(parseInt(firstArgInput.value) + parseInt(secondArgInput.value));
+        break;
+        case 'minus': 
+        setResult(parseInt(firstArgInput.value) - parseInt(secondArgInput.value));
+        break;       
+      }
+      
+    });
+  }
+
+  // minus 
+
+
+  function setResult(result) {
+    document.getElementById('result').innerHTML = result;
+  }
+  
+
+  // todo: write program which calculate value 
+  // base on input values on calculate btn click
+
+
+
+})();
