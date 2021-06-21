a47f2eb80b5ebccf118cd1a5c4d0351dc1d44195
Ewelina <ewelinajazurek@gmail.com>
2021-06-21 11:55:54 +0200

    adding devide by 0 & simplifying function

--- Javascript_course/calculator/calculator.js
+++ Javascript_course/calculator/calculator.js
@@ -6,37 +6,62 @@
   const operation = document.getElementById('operation');
 
   registerCalculateClick();
-  
-  
-  
-  
-  
+
   function registerCalculateClick() {
     const calculateBtn = document.getElementById('calculateBtn');
-    
+
+
     calculateBtn.addEventListener('click', (clickEvent) => {
-      switch(operation.value) {
-        case 'plus': 
-        setResult(parseInt(firstArgInput.value) + parseInt(secondArgInput.value));
-        break;
-        case 'minus': 
-        setResult(parseInt(firstArgInput.value) - parseInt(secondArgInput.value));
-        break;   
-        case 'multiply': 
-        setResult(parseInt(firstArgInput.value) * parseInt(secondArgInput.value));
-        break;
-        case 'divide': 
-        setResult(parseInt(firstArgInput.value) / parseInt(secondArgInput.value));
-        break;             
+
+      const firstArg = parseInt(firstArgInput.value);
+      const secondArg = parseInt(secondArgInput.value);
+
+      let resultToDisplay;
+
+      switch (operation.value) {
+        case 'plus':
+          resultToDisplay = sumCalculation(firstArg, secondArg);
+          break;
+        case 'minus':
+          resultToDisplay = subtractCalculation(firstArg, secondArg);
+          break;
+        case 'multiply':
+          resultToDisplay = multiplyCalculation(firstArg, secondArg);
+          break;
+        case 'divide':
+          resultToDisplay = devideCalculation(firstArg, secondArg);
+          break;
       }
-      
+
+      setResult(resultToDisplay);
+
     });
   }
 
   function setResult(result) {
     document.getElementById('result').innerHTML = result;
   }
-  
+
+  function sumCalculation(arg1, arg2) {
+    return arg1 + arg2;
+  }
+
+  function subtractCalculation(arg1, arg2) {
+    return arg1 - arg2;
+  }
+
+  function multiplyCalculation(arg1, arg2) {
+    return arg1 * arg2;
+  }
+
+  function devideCalculation(arg1, arg2) {
+    if (arg2 === 0) {
+      alert('you can not divide by 0');
+      return 0;
+    } else {
+      return arg1 / arg2;
+    }
+  }
 
   // todo: write program which calculate value 
   // base on input values on calculate btn click
@@ -44,3 +69,4 @@
 
 
 })();
+
--- Javascript_course/function.js
+++ Javascript_course/function.js
@@ -36,3 +36,9 @@ const circuitPower = powerCalculator(230,10);
 
 console.log(circuitPower); 
 
+
+
+// Return the Next Number from the Integer Passed
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+
