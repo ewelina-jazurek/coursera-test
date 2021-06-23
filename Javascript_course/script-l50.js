6ee56f6eaba8132c8acf4631635e30bfad8103ab
Ewelina <ewelinajazurek@gmail.com>
2021-06-23 21:11:15 +0200

    lessons 53-55

--- Javascript_course/script-l50.js
+++ Javascript_course/script-l50.js
@@ -1,4 +1,61 @@
 // DOM manipulation
-console.log(document.getElementById('title'));
+// console.log(document.getElementById('title'));
 
-console.log(document instanceof HTMLDocument);
+// console.log(document instanceof HTMLDocument);
+
+
+// Event handling
+document.addEventListener("DOMContentLoaded",
+    function (event) {
+
+        // function 
+        function sayHello(event) {
+            console.log(event);
+
+
+            this.textContent = "Said it!"
+            var name =
+                document.getElementById("name").value;
+            var message = "<h2>Hello " + name + "!</h2>";
+
+            // document
+            // .getElementById("content")
+            // .textContent = message;
+
+            document
+                .getElementById("content")
+                .innerHTML = message;
+
+            if (name === "student") {
+                var title =
+                    // document.getElementById("title")
+                    document
+                        .querySelector("#title")
+                        .textContent;
+                title += " & Lovin' it";
+                document
+                    .querySelector("#title")
+                    .textContent = title;
+            }
+
+        }
+
+
+
+        // Unobstructive event binding 
+        // document.querySelector("button")
+        //     .addEventListener("click", sayHello);
+
+        // document.querySelector("button")
+        // .onclick = sayHello;
+
+
+        document.querySelector("body")
+        addEventListener("mousemove"),
+            function (event) {
+                console.log(event.clientX);
+                console.log(event.clientY);
+            }
+
+    }
+);
