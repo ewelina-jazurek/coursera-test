20007b798b1873c0f9dc05c454168b6e467bf5a0
Ewelina <ewelinajazurek@gmail.com>
2021-07-07 09:57:01 +0200

    events

--- Javascript_course/js-crash/js-crash.js
+++ Javascript_course/js-crash/js-crash.js
@@ -345,9 +345,19 @@ var ul = document.querySelector('items');
 // ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
 
 
-const btn = document.querySelector('btn');
+var btn = document.querySelector('btn');
 // btn.style.background = 'red';
 
 
 // EVENTS 
-btn.addEventListener('click');
+
+// 1. event (click, mousehouver, mouseout) 2. function when this event happens (e = event)
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+// document.querySelector('body').classList.add('bg-dark');
+// document.querySelector('items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+// //     console.log('click');
+// });
+
+
