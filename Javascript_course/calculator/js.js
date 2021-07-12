0ad3e469b25e65ecb307f89892435d0b80ba1ddf
Ewelina <ewelinajazurek@gmail.com>
2021-07-12 20:34:49 +0200

    create js.js

---
+++ Javascript_course/calculator/js.js
@@ -0,0 +1,17 @@
+const firstName = 'Ewelina';
+
+if (firstName === 'Ewelina') {
+    var abc = 'abc';
+}
+
+function ewelina(arg1) {
+    var dd = 'dd'
+    return arg1 + dd;
+}
+
+function jazurek() {
+    return arg1;
+}
+
+console.log(abc);
+// console.log(dd);
