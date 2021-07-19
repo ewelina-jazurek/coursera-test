38f73719efd4d2304e7a8eaf621d62f871a73441
Ewelina <ewelinajazurek@gmail.com>
2021-07-19 17:08:52 +0200

    write const & functions

---
+++ Javascript_course/calculator/project-calculator.js
@@ -0,0 +1,42 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    clear() {
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandButton = document.querySelector('[data-previous-operand]')
+const currentOperandButton = document.querySelector('[data-current-operand]')
+
+
